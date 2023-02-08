5c115ca42a039bcee7b13920921c854e8293c899
FranAlonso91 <franciscojalonso91@gmail.com>
2023-02-08 19:29:24 +0100

    first commit

---
+++ src/Preguntas.js
@@ -0,0 +1,42 @@
+const Preguntas = [
+  {
+    titulo: "¿Quién es el máximo goleador de la historia del Real Madrid?",
+    opciones: [
+      { textoRespuesta: "Hugo Sánchez", isCorrect: false },
+      { textoRespuesta: "Raúl", isCorrect: false },
+      { textoRespuesta: "Cristiano Ronaldo", isCorrect: true },
+      { textoRespuesta: "Ronaldo", isCorrect: false },
+    ],
+  },
+  {
+    titulo: "¿Cuántas Liga de Campeones posee el Real Madrid?",
+    opciones: [
+      { textoRespuesta: "9", isCorrect: false },
+      { textoRespuesta: "11", isCorrect: false },
+      { textoRespuesta: "3", isCorrect: false },
+      { textoRespuesta: "13", isCorrect: true },
+    ],
+  },
+  {
+    titulo:
+      "¿Quién fué el primer jugador del Real Madrid en tener un balón de oro?",
+    opciones: [
+      { textoRespuesta: "Di Stéfano", isCorrect: true },
+      { textoRespuesta: "Figo", isCorrect: false },
+      { textoRespuesta: "Cannavaro", isCorrect: false },
+      { textoRespuesta: "Kopa", isCorrect: false },
+    ],
+  },
+  {
+    titulo:
+      "¿Quién es el jugador con más tarjetas rojas de la historia del Real Madrid?",
+    opciones: [
+      { textoRespuesta: "Walter Samuel", isCorrect: false },
+      { textoRespuesta: "Arbeloa", isCorrect: false },
+      { textoRespuesta: "Fernando Hierro", isCorrect: true },
+      { textoRespuesta: "Sergio Ramos", isCorrect: false },
+    ],
+  },
+];
+
+export default Preguntas;
